a0cb2fb4da661f97b94c3f46dbd8aacb00e370ef
akio0911 <akio0911@gmail.com>
2023-05-11 18:16:52 +0900

    feedback

--- ryu_kadai5/ViewController.swift
+++ ryu_kadai5/ViewController.swift
@@ -20,29 +20,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction private func resultButton(_ sender: Any) {
-        var number1 = Float(textField1.text ?? "")
-        var number2 = Float(textField2.text ?? "")
-        //AlertControllerの引数
-        let title = "課題5"
-        var message = ""
-        
-        if number1 == nil {
-            message = "割られる数を入力して下さい"
-        }else if number2 == nil {
-            message = "割る数を入力して下さい"
-        }else if number2 == 0 {
-            message = "割る数には0を入力しないで下さい"
-        }else {
-            resultLabel.text = "\(number1! / number2!)"
+        guard let number1 = Float(textField1.text ?? "") else {
+            presentAlert(message: "割られる数を入力して下さい")
+            return
         }
-        
-        if message != "" {
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            
-            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alert.addAction(ok)
-            
-            present(alert,animated: true, completion: nil)
+
+        guard let number2 = Float(textField2.text ?? "") else {
+            presentAlert(message: "割る数を入力して下さい")
+            return
         }
+
+        guard number2 != 0 else {
+            presentAlert(message: "割る数には0を入力しないで下さい")
+            return
+        }
+
+        resultLabel.text = "\(number1 / number2)"
+    }
+
+    private func presentAlert(message: String) {
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(ok)
+
+        present(alert,animated: true, completion: nil)
     }
 }
